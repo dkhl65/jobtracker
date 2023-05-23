cf0c513dca00e83f66c7c10f45cc76f4306d85aa
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2023-05-23 15:16:23 -0400

    Move the Dialog components to a separate file.

--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -1,12 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useMemo,
-  useReducer,
-  forwardRef,
-  useImperativeHandle,
-} from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import {
   Container,
@@ -14,8 +6,6 @@ import {
   Toolbar,
   Box,
   TextField,
-  Checkbox,
-  FormControlLabel,
   Alert,
   Paper,
   Table,
@@ -28,372 +18,14 @@ import {
   IconButton,
   TablePagination,
   Tooltip,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
 } from "@mui/material";
 import LaunchIcon from "@mui/icons-material/Launch";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import dayjs from "dayjs";
+import JobDialogs from "./JobDialogs";
 import NavBar from "./NavBar";
 
 const NEW = -1;
-const blankForm = {
-  company: "",
-  location: "",
-  link: "",
-  remote: false,
-  application: "",
-  assessment: [""],
-  interview: [""],
-  rejection: "",
-  notes: "",
-};
-
-const Dialogs = forwardRef((props, ref) => {
-  const { job, reloadJobs, axiosPrivate } = props;
-  const formData = useRef(JSON.parse(JSON.stringify(blankForm)));
-  const [, update] = useReducer((x) => x + 1, 0);
-  const [editOpen, setEditOpen] = useState(false);
-  const [deleteOpen, setDeleteOpen] = useState(false);
-  const [formMessage, setFormMessage] = useState({ error: false, message: "" });
-  const [newJob, setNewJob] = useState(true);
-
-  useImperativeHandle(ref, () => ({
-    openEditForm() {
-      const newForm = {
-        ...job,
-        assessment: job.assessment.split(","),
-        interview: job.interview.split(","),
-      };
-      formData.current = newForm;
-      setNewJob(false);
-      setEditOpen(true);
-    },
-    openNewForm() {
-      formData.current = JSON.parse(JSON.stringify(blankForm));
-      setNewJob(true);
-      setEditOpen(true);
-    },
-    openDeleteForm() {
-      setDeleteOpen(true);
-    },
-  }));
-
-  useEffect(() => {
-    setFormMessage({ error: false, message: "" });
-  }, [editOpen, deleteOpen]);
-
-  const saveJob = async (e) => {
-    e.preventDefault();
-    if (formMessage.message.length > 0) {
-      return;
-    }
-    const jobApp = {
-      ...formData.current,
-      company: formData.current.company.trim(),
-      location: formData.current.location.trim(),
-      link: formData.current.link.trim(),
-      assessment: formData.current.assessment
-        .filter((date) => date.length > 0 && date.indexOf("Invalid Date") < 0)
-        .join(","),
-      interview: formData.current.interview
-        .filter((date) => date.length > 0 && date.indexOf("Invalid Date") < 0)
-        .join(","),
-      notes: formData.current.notes.trim(),
-    };
-    try {
-      if (newJob) {
-        setFormMessage({ error: false, message: "Adding job application..." });
-        await axiosPrivate.post("/jobs", jobApp);
-      } else {
-        setFormMessage({ error: false, message: "Saving..." });
-        await axiosPrivate.put("/jobs", jobApp);
-      }
-      setEditOpen(false);
-      reloadJobs();
-    } catch (err) {
-      if (!err?.response) {
-        setFormMessage({ error: true, message: "No server response." });
-      } else if (err.response?.status === 500) {
-        setFormMessage({
-          error: true,
-          message: `Internal server error: ${
-            err.response.data?.name || "unknown error"
-          }. Please try again later.`,
-        });
-      } else {
-        setFormMessage({
-          error: true,
-          message: err.response?.data?.message || err.message,
-        });
-      }
-    }
-  };
-
-  const deleteJob = (e) => {
-    e.preventDefault();
-    if (formMessage.message.length > 0 || !job?.id) {
-      return;
-    }
-    setFormMessage({ error: false, message: "Deleting job application..." });
-    axiosPrivate
-      .delete("/jobs", { data: { id: job.id } })
-      .then(() => {
-        setDeleteOpen(false);
-        reloadJobs();
-      })
-      .catch((err) => {
-        if (!err?.response) {
-          setFormMessage({ error: true, message: "No server response." });
-        } else if (err.response?.status === 500) {
-          setFormMessage({
-            error: true,
-            message: `Internal server error: ${
-              err.response.data?.name || "unknown error"
-            }. Please try again later.`,
-          });
-        } else {
-          setFormMessage({
-            error: true,
-            message: err.response?.data?.message || err.message,
-          });
-        }
-      });
-  };
-
-  return (
-    <>
-      <Dialog
-        open={editOpen}
-        onClose={() => {
-          if (formMessage.error || formMessage.message.length === 0) {
-            setEditOpen(false);
-          }
-        }}
-      >
-        <Box component="form" onSubmit={saveJob}>
-          <DialogTitle>
-            {(!newJob && `Edit ${job?.company} Application`) || "Add Job"}
-          </DialogTitle>
-          <DialogContent>
-            <TextField
-              label="Company"
-              fullWidth
-              variant="standard"
-              type="text"
-              margin="dense"
-              defaultValue={formData.current.company}
-              onChange={(e) => (formData.current.company = e.target.value)}
-              inputProps={{ maxLength: 255 }}
-              required
-            />
-            <TextField
-              label="Location"
-              fullWidth
-              variant="standard"
-              type="text"
-              margin="dense"
-              defaultValue={formData.current.location}
-              onChange={(e) => (formData.current.location = e.target.value)}
-              inputProps={{ maxLength: 255 }}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={formData.current.remote}
-                  onChange={(e) => {
-                    formData.current.remote = e.target.checked;
-                    update();
-                  }}
-                />
-              }
-              label="Remote"
-            />
-            <TextField
-              label="Link to Job Post"
-              fullWidth
-              variant="standard"
-              type="url"
-              margin="dense"
-              defaultValue={formData.current.link}
-              onChange={(e) => (formData.current.link = e.target.value)}
-              inputProps={{ maxLength: 255 }}
-            />
-            <DatePicker
-              format="YYYY-MM-DD"
-              margin="dense"
-              label="Application Date"
-              defaultValue={
-                formData.current.application.length > 0
-                  ? dayjs(formData.current.application)
-                  : null
-              }
-              onChange={(e) =>
-                (formData.current.application = e ? e.format("YYYY-MM-DD") : "")
-              }
-              sx={{ mt: "15px", mr: "15px" }}
-            />
-            <br />
-            {formData.current.assessment.map((value, index) => (
-              <Box key={index}>
-                <DatePicker
-                  format="YYYY-MM-DD"
-                  margin="dense"
-                  label={`Technical Assessment ${index + 1}`}
-                  defaultValue={value.length > 0 ? dayjs(value) : null}
-                  onChange={(e) =>
-                    (formData.current.assessment[index] = e
-                      ? e.format("YYYY-MM-DD")
-                      : "")
-                  }
-                  sx={{ mt: "15px" }}
-                />
-                {formData.current.assessment.length > 1 && (
-                  <Tooltip title="Remove technical assessment date">
-                    <IconButton
-                      sx={{ mt: "22px" }}
-                      onClick={() => {
-                        formData.current.assessment.splice(index, 1);
-                        update();
-                      }}
-                    >
-                      <RemoveIcon />
-                    </IconButton>
-                  </Tooltip>
-                )}
-                {Number(index) === formData.current.assessment.length - 1 && (
-                  <Tooltip title="Add another technical assessment date">
-                    <IconButton
-                      sx={{ mt: "22px" }}
-                      onClick={() => {
-                        formData.current.assessment.push("");
-                        update();
-                      }}
-                    >
-                      <AddIcon />
-                    </IconButton>
-                  </Tooltip>
-                )}
-                <br />
-              </Box>
-            ))}
-            {formData.current.interview.map((value, index) => (
-              <Box key={index}>
-                <DatePicker
-                  format="YYYY-MM-DD"
-                  margin="dense"
-                  label={`Interview ${index + 1}`}
-                  defaultValue={value.length > 0 ? dayjs(value) : null}
-                  onChange={(e) =>
-                    (formData.current.interview[index] = e
-                      ? e.format("YYYY-MM-DD")
-                      : "")
-                  }
-                  sx={{ mt: "15px" }}
-                />
-                {formData.current.interview.length > 1 && (
-                  <Tooltip title="Remove interview date">
-                    <IconButton
-                      sx={{ mt: "22px" }}
-                      onClick={() => {
-                        formData.current.interview.splice(index, 1);
-                        update();
-                      }}
-                    >
-                      <RemoveIcon />
-                    </IconButton>
-                  </Tooltip>
-                )}
-                {Number(index) === formData.current.interview.length - 1 && (
-                  <Tooltip title="Add another interview date">
-                    <IconButton
-                      sx={{ mt: "22px" }}
-                      onClick={() => {
-                        formData.current.interview.push("");
-                        update();
-                      }}
-                    >
-                      <AddIcon />
-                    </IconButton>
-                  </Tooltip>
-                )}
-                <br />
-              </Box>
-            ))}
-            <DatePicker
-              format="YYYY-MM-DD"
-              margin="dense"
-              label="Rejection Date"
-              defaultValue={
-                formData.current.rejection.length > 0
-                  ? dayjs(formData.current.rejection)
-                  : null
-              }
-              onChange={(e) =>
-                (formData.current.rejection = e ? e.format("YYYY-MM-DD") : "")
-              }
-              sx={{ mt: "15px" }}
-            />
-            <br />
-            <TextField
-              label="Notes"
-              variant="standard"
-              type="text"
-              margin="dense"
-              defaultValue={formData.current.notes}
-              onChange={(e) => (formData.current.notes = e.target.value)}
-              inputProps={{ maxLength: 1000 }}
-              fullWidth
-              multiline
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button type="submit">{newJob ? "Add" : "Save"}</Button>
-            <Button onClick={() => setEditOpen(false)}>Cancel</Button>
-          </DialogActions>
-          {formMessage.message.length > 0 && (
-            <Alert severity={formMessage.error ? "error" : "info"}>
-              {formMessage.message}
-            </Alert>
-          )}
-        </Box>
-      </Dialog>
-      <Dialog
-        open={deleteOpen}
-        onClose={() => {
-          if (formMessage.error || formMessage.message.length === 0) {
-            setDeleteOpen(false);
-          }
-        }}
-      >
-        <DialogTitle>Delete {job?.company} Application</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to delete your application to {job?.company}?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={deleteJob}>Yes</Button>
-          <Button onClick={() => setDeleteOpen(false)}>No</Button>
-        </DialogActions>
-        {formMessage.message.length > 0 && (
-          <Alert severity={formMessage.error ? "error" : "info"}>
-            {formMessage.message}
-          </Alert>
-        )}
-      </Dialog>
-    </>
-  );
-});
 
 function Jobs() {
   const axiosPrivate = useAxiosPrivate();
@@ -471,7 +103,7 @@ function Jobs() {
   }, [dialogAction]);
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <>
       <NavBar />
       <Container maxWidth="lg" sx={{ mt: "10px", mb: "10px" }}>
         <Toolbar>
@@ -663,13 +295,12 @@ function Jobs() {
           )}
         </Box>
       </Container>
-      <Dialogs
+      <JobDialogs
         job={visibleRows[dialogAction.jobNumber]}
         ref={dialogRef}
         reloadJobs={reloadJobs}
-        axiosPrivate={axiosPrivate}
       />
-    </LocalizationProvider>
+    </>
   );
 }
 
