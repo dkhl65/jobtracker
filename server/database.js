7cf8990b0a82367151a1f0bb46c0420f4ccbf718
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2023-06-03 13:33:06 -0400

    Require pg in database.js

--- server/database.js
+++ server/database.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: require("pg"),
+});
 
 module.exports = sequelize;
